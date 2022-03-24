491f28bdf6c8a0f2d3b45d90871d5488554179fc
celiorscosta <celiorscosta@hotmail.com>
2022-03-24 09:28:21 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:admob_flutter/presentation/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admob Monetization - Banner & Interstitial Ads',
+      home: HomePage(),
+    );
+  }
+}
